ebd19b55fc87e860c4043ee7e08b912362649075
Cory Mawhorter <cory.mawhorter@gmail.com>
2016-12-04 16:18:52 -0800

    not playing nice with json-loader

--- index.js
+++ index.js
@@ -5,9 +5,15 @@
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var parsed = typeof source === "string" ? JSON.parse(source) : source;
-	var value = {};
-	for (var k in parsed) {
-		if (k[0] !== "_") value[k] = parsed[k];
+	var value;
+	if (/\bpackage\.json$/.test(this.resourcePath)) {
+		value = {};
+		for (var k in parsed) {
+			if (k[0] !== "_") value[k] = parsed[k];
+		}
+	}
+	else {
+		value = parsed;
 	}
 	this.value = [value];
 	return "module.exports = " + JSON.stringify(value) + ";";
